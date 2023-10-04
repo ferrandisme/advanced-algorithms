12a18e2e030c8341643a1d162af9d108cf8b9ac1
aferrandis <aferrandis@edicomgroup.com>
2023-10-04 16:34:54 +0200

    [FIX] KMP [Link a la tarea kmp](https://apps.edicomgroup.com/JDev/#idstask|kmp) /label ~"status::needs triage" /assign me

--- src/main/kotlin/algorithms/kmp/KnuthMorrisPrattImpl.kt
+++ src/main/kotlin/algorithms/kmp/KnuthMorrisPrattImpl.kt
@@ -9,17 +9,15 @@ class KnuthMorrisPrattImpl : KnuthMorrisPratt {
      * This aims to be used by Knuth-Morris-Pratt algorithm
      */
     override fun computeLSPTable(pattern: List<Char>): List<Int> {
-        val lsp = ArrayList<Int>(pattern.size)
+        val lsp = MutableList(pattern.size) { 0 }
         var j = 0;
-        for( patternIndex in pattern.indices){
+        for(patternIndex in 1..< pattern.size){
             while(j > 0 && pattern[patternIndex] != pattern[j]){
                 j = lsp[j-1]
             }
             if(pattern[patternIndex]== pattern[j]){
                 lsp[patternIndex] = j + 1
                 j++
-            }else{
-                lsp[patternIndex] = 0
             }
         }
         return lsp
